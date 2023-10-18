5d847c65759d931ed2702476b643d17f919622ec
ShahriarMostafiz <shahriarmostafiz19@gmail.com>
2023-10-18 22:47:54 +0600

    cart database created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // assignment-man
 // OVlN4NWU9WhSyZyl
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vfiwhpa.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,6 +30,8 @@ async function run() {
     const database = client.db("Assignment10DB");
     const brandsCollection = database.collection("brands");
     const productsCollection = database.collection("products");
+    const usersCollection = database.collection("users");
+    const CartsCollection = database.collection("carts");
     //brands post
     app.post("/brands", async (req, res) => {
       const brand = req.body;
@@ -37,14 +39,38 @@ async function run() {
       const result = await brandsCollection.insertOne(brand);
       res.send(result);
     });
+    app.put("/brands/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const data = req.body;
+      console.log(data);
+      console.log(brand, "is the brand ");
+      const filter = { brand: brand };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          img1: data.img1,
+          img2: data.img2,
+          img3: data.img3,
+        },
+      };
+      const result = await brandsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
     app.get("/brands", async (req, res) => {
       const result = await brandsCollection.find().toArray();
       res.send(result);
     });
-    // app.get("/brands/:brand", async (req, res) => {
-    //   const result = await brandsCollection.find().toArray();
-    //   res.send(result);
-    // });
+    app.get("/brands/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const query = { brand: brand };
+
+      const result = await brandsCollection.findOne(query);
+      res.send(result);
+    });
 
     // product post
     app.post("/products", async (req, res) => {
@@ -66,7 +92,35 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    // get single product
+    app.get("/products/details/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
+
+    //users management
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cart = req.body;
+      // console.log(cart);
+      const result = await CartsCollection.insertOne(cart);
+      res.send(result);
+    });
+    app.get("/carts/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await CartsCollection.find(query).toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
